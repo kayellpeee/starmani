ccd81a6b58c6bf88325931815d25ab20b916581c
Keenan Lidral-Porter <klp2012@gmail.com>
2014-10-02 21:58:15 -0700

    plays sound on load

--- server.js
+++ server.js
@@ -6,13 +6,10 @@ var port = process.env.PORT || 4000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/star', function(req, res){
 
   console.log('a repo was starred !');
-  var audio = document.getElementByTagName('audio')[0];
-  audio.play();
-  res.end();
-
+  res.redirect('/');
 
 });
 
